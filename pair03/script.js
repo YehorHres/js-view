97a9613b3491d0108ba8bd2f51ab531405a09843
Yehor Hres <yehorhres07@gmail.com>
2026-09-09 11:11:01 +0300

    Add files via upload

---
+++ pair03/script.js
@@ -0,0 +1,91 @@
+// let age = prompt('Enter your age');
+// let access = confirm("Are you sure?");
+//
+// if(age >= 18 && access === True){
+//     alert("Welcome!!!!");
+// }else{
+//     alert("Acces denied");
+// }
+
+// let role = prompt("What is your role?");
+//
+// if (role === "admin"  || role === "teacher") {
+//     alert("Welcome");
+// } else {
+//     alert("Denied");
+// }
+
+// let login = confirm("Are you registered?");
+// let age = Number(prompt("What is your age?"));
+//
+// if (login === true) {
+//     if (age >= 18) {
+//         alert("Access allowed");
+//     } else {
+//         alert("Access denied");
+//     }
+// } else {
+//      alert("Not logged in")
+//
+//
+// }
+
+
+// let age = Number(prompt("Enter your age"));
+
+// if (age < 6) {
+//     alert("Дитина");
+// } else if (age >= 6 && age <= 13) {
+//     alert("Школяр")
+//
+// }else if (age >= 14 && age <= 18) {
+//     alert("Підліток")
+// }else {
+//     alert("Дорослий")
+// }
+
+// if (age >=18) {
+//     alert("Дорослий");
+// } else if (age >= 14) {
+//     alert("Піділток")
+//
+// }else if (age >= 6) {
+//     alert("Школота")
+// }else {
+//     alert("Дитина")
+// }
+
+
+// let age = prompt('Enter your age');
+//
+// if (age >= 18) {
+//     alert('access allowed');
+// }else if(age < 18 && age >= 16){
+//     let access = confirm("Do you have access?");
+//     if (access) {
+//         alert("access allowed");
+//     }else{
+//         alert("ban");
+//     }
+// }else{
+//     alert("ban ");
+// }
+
+
+
+// const sale1 = 0.1;
+//
+//
+// let name = prompt("What is the product name?");
+// let price = Number(prompt("Price?"));
+// let quantity = Number(prompt("Quantity?"));
+// let sign = confirm("Are you signed in?");
+// let promo = prompt("Have promo?");
+// let VIP = confirm("status?");
+// const promo2 = "sale";
+// if (price * quantity >= 1000 && sign === true && (promo === promo2 || VIP === true) {
+//     alert(`Price w sale: ${price*quantity - price*quantity*sale1} uah`);
+//
+// }else{
+//     alert(`No sale price: ${price*quantity} uah`);
+// }
